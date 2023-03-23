059ff87903dfe9a9f47ab6dc34f66c150b29c79e
Cristian Rodriguez <an1me@hotmail.es>
2023-03-23 07:38:29 -0500

    practica react

---
+++ src/helloApp.jsx
@@ -0,0 +1,11 @@
+/* export function App() {
+  return <h1>Hola mundo!!!</h1>;
+} */
+
+//! se pued reducir la funcion pero por lo general se utiliza
+// export const HelloApp = () => <h1>Hola Cristian</h1>;
+
+//! utilizar siempre PascalCase
+export const HelloApp = () => {
+  return <h1>hello App</h1>;
+};
